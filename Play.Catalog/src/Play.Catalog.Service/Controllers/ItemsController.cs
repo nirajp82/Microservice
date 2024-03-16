c31a36b95cbeccb2254578a2c69015adafd1c016
Niraj Patel <nirajp82@yahoo.com>
2024-03-16 15:43:19 -0400

    Use Repo for CRUD Operations

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
+using Play.Catalog.Service.Entities;
+using Play.Catalog.Service.Reposiroty;
 
 namespace Play.Catalog.Service.Controllers;
 
@@ -7,73 +11,79 @@ namespace Play.Catalog.Service.Controllers;
 [Route("items")]
 public class ItemsController : ControllerBase
 {
-    private static readonly List<ItemDto> items = new()
-    {
-        new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
-        new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
-        new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
-    };
+    public readonly ItemsRepository _itemsRepository = new();
 
     [HttpGet]
     [ProducesResponseType<IEnumerable<ItemDto>>(StatusCodes.Status200OK)]
-    public IResult Get()
+    public async Task<IResult> GetAsync()
     {
-        return Results.Ok(items);
+        var result = (await _itemsRepository.GetAllAsync())
+                        .Select(Item => Item.AsDto());
+        return Results.Ok(result);
     }
 
     // GET /items/{id}
     [HttpGet("{id}")]
     [ProducesResponseType<ItemDto>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IResult GetById(Guid id)
+    public async Task<IResult> GetByIdAsync(Guid id)
     {
-        var item = items.Where(item => item.Id == id).FirstOrDefault();
-        if (item != null)
+        var item = await _itemsRepository.GetAsync(id);
+        if (item == null)
         {
-            return Results.Ok(item);
+            return Results.NotFound();
         }
-        return Results.NotFound();
+        return Results.Ok(item.AsDto());
     }
 
     [HttpPost]
     [ProducesResponseType<ItemDto>(StatusCodes.Status201Created)]
-    public IActionResult Post(CreateItemDto createItemDto)
+    public async Task<IActionResult> PostAsync(CreateItemDto createItemDto)
     {
-        var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
-        items.Add(item);
-        return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        var item = new Item
+        {
+            Id = Guid.NewGuid(),
+            Name = createItemDto.Name,
+            Description = createItemDto.Description,
+            Price = createItemDto.Price,
+            CreatedDate = DateTimeOffset.UtcNow
+        };
+        await _itemsRepository.CreateAsync(item);
+        return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, createItemDto);
     }
 
     // PUT /items/{id}
     [HttpPut("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+    public async Task<IActionResult> PutAsync(Guid id, UpdateItemDto updateItemDto)
     {
-        var existingItem = items.Where(item => item.Id == id).FirstOrDefault();
-        if (existingItem != null)
+        var existingItem = await _itemsRepository.GetAsync(id);
+        if (existingItem == null)
         {
-            var updatedItem = existingItem with
-            {
-                Name = updateItemDto.Name,
-                Description = updateItemDto.Description,
-                Price = updateItemDto.Price
-            };
-
-            var index = items.FindIndex(existingItem => existingItem.Id == id);
-            items[index] = updatedItem;
-            return NoContent();
+            return NotFound();
         }
-        return NotFound();
+
+        existingItem.Name = updateItemDto.Name;
+        existingItem.Description = updateItemDto.Description;
+        existingItem.Price = updateItemDto.Price;
+
+        await _itemsRepository.UpdateAsync(existingItem);
+        return NoContent();
     }
 
     // DELETE /items/{id}
     [HttpDelete("{id}")]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult Delete(Guid id)
+    public async Task<IActionResult> Delete(Guid id)
     {
-        var index = items.FindIndex(existingItem => existingItem.Id == id);
-        items.RemoveAt(index);
+        var existingItem = await _itemsRepository.GetAsync(id);
+        if (existingItem == null)
+        {
+            return NotFound();
+        }
+
+        await _itemsRepository.RemoveAsync(id);
         return NoContent();
     }
 }
--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b77abd752bfb682a485f5455aa3aebba222f9bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dabf85244477e76ef3826a378edf189eb196ff33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
