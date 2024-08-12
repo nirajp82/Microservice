6e2d9f6fa817b207d3cb7f4eaa9bd9dfb37eeb54
Niraj Patel <nirajp82@yahoo.com>
2024-08-11 20:04:05 -0400

    Completed Securing the Inventory microservice

--- Play.Common/src/Play.Common/Identity/Extensions.cs
+++ Play.Common/src/Play.Common/Identity/Extensions.cs
@@ -11,8 +11,17 @@ public static class Extensions
 {
     public static AuthenticationBuilder AddJwtBearerAuthentication(this IServiceCollection services) 
     {
-        return services.ConfigureOptions<ConfigureJwtBearerOptions>()
-                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer();
+        return services
+            // Registers the ConfigureJwtBearerOptions class to configure JwtBearerOptions.
+            // This ensures that JwtBearerOptions are properly set up according to the configuration provided in the ConfigureJwtBearerOptions class.
+            .ConfigureOptions<ConfigureJwtBearerOptions>()
+
+            // Adds authentication services to the ASP.NET Core application.
+            // Specifies that JWT Bearer tokens will be used for authentication, using the default authentication scheme "Bearer".
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+            // Configures the JWT Bearer token authentication middleware.
+            // This middleware will handle the authentication process for incoming requests that include a JWT Bearer token.
+            .AddJwtBearer();
     }
 }
